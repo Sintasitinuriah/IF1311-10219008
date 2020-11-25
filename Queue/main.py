85240aea62b3b27dabe52f9ca19fe8e8017237be
Sintasitinuriah <72982282+Sintasitinuriah@users.noreply.github.com>
2020-11-25 11:08:38 +0700

    Update main.py

--- Queue/main.py
+++ Queue/main.py
@@ -26,15 +26,4 @@ print('data sekarang: ',antrian)
 # cek ukuran antrian
 print(len(antrian))
 
-# implementasi queue tanpa deque
-antrian2 =[1,2,3]
-print('data sekarang: ',antrian2)
 
-# tambah / add/ enqueue
-antrian2.append(4)
-print('data antrian sekarang', antrian2)
-
-# kurang / delete / dequeque
-out = antrian2.pop(0)
-print('data keluar : ',out)
-print('data sekarang : ',antrian2)
